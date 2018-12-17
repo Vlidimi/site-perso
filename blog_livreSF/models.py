c426a59d97fc019528e91a175803a4f7ba9a5b03
valentin <valentin.anfray@gmail.com>
2018-12-17 19:51:18 +0100

    Add resize img

--- blog_livreSF/models.py
+++ blog_livreSF/models.py
@@ -38,7 +38,30 @@ class Article(models.Model):
 
     def save(self, *args, **kwargs):
         self.slug= slugify(self.titre) #Permet de générer un slug à partir d'une chaine de caractères
-        super(Article, self).save(*args, **kwargs)    
+        if self.photo: # Evite le cas où l'image est celle par défaut
+            img = Image.open(self.photo) # Pour changer la dimension d'une image enregistrée pour ne pas qu'elle prenne trop de place dans la bdd
+            size_max_img = 40000 #Taille maximale de l'image
+            ratio = self.photo.width/self.photo.height # Pour conserver les mêmes dimensions de l'image
+            rescalled_width = int((ratio*size_max_img)**0.5)
+            rescalled_height = int((size_max_img/ratio)**0.5)
+            resized  = img.resize((rescalled_width, rescalled_height), Image.ANTIALIAS)
+            new_image_io = BytesIO()
+
+            if img.format == 'JPEG' :
+                resized.save(new_image_io, format='JPEG')
+            elif img.format == 'PNG' :
+                resized.save(new_image_io, format='PNG')
+
+            temp_name = self.photo.name
+            self.photo.delete(save=False)
+
+            self.photo.save(
+                temp_name,
+                content=ContentFile(new_image_io.getvalue()),
+                save=False
+            )
+        super(Profile, self).save(*args, **kwargs) 
+
     
     class Meta:
         verbose_name = "article"
--- connexion/models.py
+++ connexion/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.template.defaultfilters import slugify
 
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
+from django.core.files.images import get_image_dimensions
+
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) 
@@ -21,6 +27,28 @@ class Profile(models.Model):
 
     def save(self, *args, **kwargs):
         self.slug_pseudo= slugify(self.pseudo) #Permet de générer un slug à partir d'une chaine de caractères
+        if 'nobody.png' not in self.avatar.url: # Evite le cas où l'image est celle par défaut
+            img = Image.open(self.avatar) # Pour changer la dimension d'une image enregistrée pour ne pas qu'elle prenne trop de place dans la bdd
+            size_max_img = 40000 #Taille maximale de l'image
+            ratio = self.avatar.width/self.avatar.height # Pour conserver les mêmes dimensions de l'image
+            rescalled_width = int((ratio*size_max_img)**0.5)
+            rescalled_height = int((size_max_img/ratio)**0.5)
+            resized  = img.resize((rescalled_width, rescalled_height), Image.ANTIALIAS)
+            new_image_io = BytesIO()
+
+            if img.format == 'JPEG' :
+                resized.save(new_image_io, format='JPEG')
+            elif img.format == 'PNG' :
+                resized.save(new_image_io, format='PNG')
+
+            temp_name = self.avatar.name
+            self.avatar.delete(save=False)
+
+            self.avatar.save(
+                temp_name,
+                content=ContentFile(new_image_io.getvalue()),
+                save=False
+            )
         super(Profile, self).save(*args, **kwargs) 
 
 class MailBox(models.Model):
